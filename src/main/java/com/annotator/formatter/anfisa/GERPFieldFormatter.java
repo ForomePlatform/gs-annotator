b9a16423c56fbc0fa6baa8f02cb154ff198b9bde
shavtvalishvili <gshavtvalishvili@gmail.com>
2024-05-28 19:10:38 +0400

    add "gerp_score" field support

---
+++ src/main/java/com/annotator/formatter/anfisa/GERPFieldFormatter.java
@@ -0,0 +1,45 @@
+package com.annotator.formatter.anfisa;
+
+import com.annotator.formatter.Formatter;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@SuppressWarnings("unused")
+public class GERPFieldFormatter implements Formatter {
+    private final JsonObject anfisaJson;
+    private final JsonObject variant;
+    private final Map<String, Object> gerpKeyMap;
+
+    public GERPFieldFormatter(JsonObject variant, Map<String, Object> preprocessedData, JsonObject anfisaJson) {
+        this.variant = variant;
+        this.anfisaJson = anfisaJson;
+        this.gerpKeyMap = getGerpKeyMap();
+        this.preprocessData();
+    }
+
+    private void preprocessData() {
+    }
+
+    private Map<String, Object> getGerpKeyMap() {
+        return new HashMap<>() {{
+            put("gerp_score", (Function<JsonObject, Integer>) (JsonObject variant) -> {
+                JsonArray gerpArray = variant.getJsonArray("GERP");
+                if (gerpArray.isEmpty()) {
+                    return null;
+                }
+
+                JsonArray gerpObject = gerpArray.getJsonArray(0);
+
+                return Integer.valueOf(gerpObject.getString(0));
+            });
+        }};
+    }
+
+    public void formatData() {
+        Formatter.formatData(gerpKeyMap, anfisaJson, variant);
+    }
+}
